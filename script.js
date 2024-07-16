f1d1247af1da84eb475426969ed7d5d4d05bda65
PriyansRaj <230701403@rajalakshmi.edu.in>
2024-07-16 17:14:54 +0530

    Removed eval and used operate function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Array.from(button).forEach((button)=>{
     button.addEventListener("click",(e)=>{
         if(e.target.innerText=='='){
            
-            string = eval(string)
+            string = operate()
         }
         else if(e.target.innerText == "AC"){
             string=""
@@ -25,3 +25,46 @@ Array.from(button).forEach((button)=>{
      screen.innerText = string
     })
 })
+
+function operate(){
+    const operators = ['+','-','*','/','%']
+    let a="",b="",op="";
+    let i=0;
+    for(i =0;i<string.length;i++){
+        console.log(string[i]);
+        if(operators.includes(string[i]) && a.length>0) {
+            break;
+        }
+    else{
+        a+= string[i]
+    }
+    }
+    op = string[i]
+    a= Number(a)
+    b = Number(string.slice(i+1,))
+    string =""
+    console.log(`a = ${a} op=${op} b =${b}`)
+    switch(op){
+        case '+':{
+            string = a+b;
+            break;
+        }
+        case '-':{
+            string = a-b
+            break;
+        }
+        case '/':{
+            string = Math.round(a/b)
+            break;
+        }
+        case '*':{
+            string = a*b
+            break;
+        }
+        case '%':{
+            string = a%b
+            break;
+        }
+    }
+    return string
+}
